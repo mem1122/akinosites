d199428b987dcdec3d7eaef14bb8f87a1d3a2895
mem11 <rekrut337@gmail.com>
2026-04-17 21:10:01 +0300

    setup firebase

--- Client/src/firebase.js
+++ Client/src/firebase.js
@@ -2,8 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const app = initializeApp({
-  apiKey: "ТВОЙ_API_KEY",
-  databaseURL: "ТВОЙ_DB_URL"
+  apiKey: "AIzaSyBgmbhOr5G2M9dHjt8fklpy6OeWLSXhMmw",
+  databaseURL: "https://console.firebase.google.com/u/0/project/akinostaff/database/akinostaff-default-rtdb/data/~2F"
 });
 
 export const db = getDatabase(app);
