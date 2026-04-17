ce593ad21ba4ce26ddd1147ffdec7e169561d94a
mem11 <rekrut337@gmail.com>
2026-04-17 21:51:19 +0300

    Fix auth

--- server/index.js
+++ server/index.js
@@ -12,13 +12,17 @@ app.get("/check-access/:id",(req,res)=>{
   res.json({ access: access[req.params.id] || false });
 });
 
+// 🔐 Discord login
+app.get("/auth/discord", (req, res) => {
+  const url = `https://discord.com/oauth2/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&redirect_uri=${process.env.REDIRECT_URI}&scope=identify`;
+  res.redirect(url);
+});
+
 app.post("/buy",async(req,res)=>{
   const item=req.body;
-
   await axios.post("WEBHOOK_URL",{
     content:`Покупка: ${item.name}`
   });
-
   res.json({ok:true});
 });
 
