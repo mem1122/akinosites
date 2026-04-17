d7706053e13e3d562f063d9bd490c621644cb72b
mem11 <rekrut337@gmail.com>
2026-04-17 22:25:28 +0300

    Fix inde"

--- server/index.js
+++ server/index.js
@@ -27,3 +27,46 @@ app.post("/buy",async(req,res)=>{
 });
 
 app.listen(3001);
+app.get("/auth/callback", async (req, res) => {
+  const code = req.query.code;
+
+  try {
+    const tokenRes = await axios.post(
+      "https://discord.com/api/oauth2/token",
+      new URLSearchParams({
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        grant_type: "authorization_code",
+        code,
+        redirect_uri: process.env.REDIRECT_URI
+      }),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      }
+    );
+
+    const token = tokenRes.data.access_token;
+
+    const userRes = await axios.get("https://discord.com/api/users/@me", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    const user = userRes.data;
+
+    access[user.id] = true;
+
+    res.send(`
+      <script>
+        localStorage.setItem("id", "${user.id}");
+        window.location.href = "/";
+      </script>
+    `);
+  } catch (err) {
+    console.error(err.response?.data || err);
+    res.send("Ошибка авторизации");
+  }
+});
